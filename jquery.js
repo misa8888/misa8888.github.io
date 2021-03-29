74c5e53fc64c59a3c440d5c4ce8bd6377b29c087
misa8888 <79821791+misa8888@users.noreply.github.com>
2021-03-30 08:02:28 +0900

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,18 +1,30 @@
 $(function() {
-  $('#past-show').on('click', function() {
+  $('.past-show').on('click', function() {
     $('#past-modal').fadeIn();
   });
 
-  $('#current-show').on('click', function() {
+  $('.current-show').on('click', function() {
     $('#current-modal').fadeIn();
   });
 
-  $('#future-show').on('click', function() {
+  $('.future-show').on('click', function() {
     $('#future-modal').fadeIn();
   });
 
+  $('.major-show').on('click', function() {
+    $('#major-modal').fadeIn();
+  });
+
+  $('.archery-show').on('click', function() {
+    $('#archery-modal').fadeIn();
+  });
+
+  $('.study-show').on('click', function() {
+    $('#study-modal').fadeIn();
+  });
+
   $('.close-modal').on('click', function(){
-    $("#past-modal, #current-modal, #future-modal").fadeOut();
+    $("#past-modal, #current-modal, #future-modal, #major-modal, #archery-modal, #study-modal").fadeOut();
   });
 
 });
